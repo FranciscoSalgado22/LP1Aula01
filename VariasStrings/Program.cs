9d344bbc8123129540641f579678f00b0d5d74d2
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-02-27 17:31:55 +0000

    Add VariasStrings strings com tipos diferentes

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string sn = "Normal String";
+            string sd = "Esta string\n recebe um enter e\t executa um tab";
+            string slink = @"C:\Users\a21902823\Desktop\LP1\LP1Aula1";
+            string suni = "Esta string tem um unicode - \u00A3";
+            Console.WriteLine(sn);
+            Console.WriteLine(sd);
+            Console.WriteLine(slink);
+            Console.WriteLine(suni);
+        }
+    }
+}
