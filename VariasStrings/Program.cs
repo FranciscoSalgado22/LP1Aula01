5281de2eab20407269005b85046ca18d65345c6c
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-02-27 17:55:31 +0000

    Add VariasStrings strings concat, interpolada e format

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -16,6 +16,21 @@ namespace VariasStrings
             Console.WriteLine(sd);
             Console.WriteLine(slink);
             Console.WriteLine(suni);
+            int x = 50;
+            int y = 64;
+            string sc = "Ola " + 2 + " como é";
+            string scx = "Numero " + x;
+            Console.WriteLine(sc);
+            Console.WriteLine(scx);
+            sc += scx;
+            Console.WriteLine(sc);
+            string sci = $"O valor de y é {y}";
+            string scim = $"{x} vezes {y} é igual a {x*y}";
+            Console.WriteLine(sci);
+            Console.WriteLine(scim);
+            string sf = String.Format("Gosto de {0} e do nº {1}", "batata", 12);
+            Console.WriteLine(sf);
+            Console.WriteLine("Vou a {0} e o numero de y é {1}", "praia", y);
         }
     }
 }
