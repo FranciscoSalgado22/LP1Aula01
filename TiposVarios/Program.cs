6569fc5bfe3aa051eeb72bcea7040bfcd471ce76
Francisco Salgado <a21902823@alunos.ulht.pt>
2020-02-27 17:17:20 +0000

    Add TiposVarios variaveis int

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = -10;
+            long varl = 20L;
+            uint varu = 10U;
+            Console.WriteLine(var);
+            Console.WriteLine(varl);
+            Console.WriteLine(varu);
+            char a = '\u0071';
+            char b = '\u003E';
+            char c = '\u0031';
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
